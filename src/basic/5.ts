af73f1be1633faa05c7e22a97303b8e540d87ff6
gWake3007 <81764484+gWake3007@users.noreply.github.com>
2024-07-11 13:57:29 +0200

    1

--- src/basic/5.ts
+++ src/basic/5.ts
@@ -8,4 +8,10 @@ enum DayOfWeek {
   Sunday,
 }
 
-const isWeekend = (day) => {};
+const isWeekend = (day: DayOfWeek): boolean => {
+  return day === DayOfWeek.Saturday || day === DayOfWeek.Sunday;
+};
+
+console.log(isWeekend(DayOfWeek.Monday));
+console.log(isWeekend(DayOfWeek.Saturday));
+console.log(isWeekend(DayOfWeek.Sunday));
