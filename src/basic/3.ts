eb37fb54d4035e5843cab19d91a4a4dd17af8749
gWake3007 <81764484+gWake3007@users.noreply.github.com>
2024-07-11 11:04:26 +0200

    1

---
+++ src/basic/3.ts
@@ -0,0 +1,8 @@
+let mixedType: string | number;
+mixedType = "string";
+mixedType = 33;
+
+type literalExample = "enable" | "disable";
+let value: literalExample;
+value = "enable";
+value = "disable";
